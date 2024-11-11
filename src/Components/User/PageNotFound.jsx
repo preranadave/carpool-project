df9e79d7ae519b1d5cf7d469f189bd4215275142
preranadave <psdave.3399@gmail.com>
2024-11-12 00:31:51 +0530

    title ccommit

--- src/Components/User/PageNotFound.jsx
+++ src/Components/User/PageNotFound.jsx
@@ -1,5 +1,5 @@
 import React from "react";
 
 export default function PageNotFound() {
-  return <h1 className="text-center text-3xl absolute top-1/2 left-1/2">Sorry Page Not Found!</h1>;
+  return <h1 className="text-3xl absolute top-1/2 left-1/2">Sorry Page Not Found!</h1>;
 }
